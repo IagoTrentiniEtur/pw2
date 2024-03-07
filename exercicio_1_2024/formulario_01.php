9a549086875be1c48fba60d619c7118a24245038
Iago Trentini Etur <iago@arealocal.com.br>
2024-03-07 20:50:52 -0300

    exercicio 1

---
+++ exercicio_1_2024/formulario_01.php
@@ -0,0 +1,6 @@
+<?php
+    echo "|Nome: " . $_REQUEST['nome'];
+    echo "|Telefone: " . $_REQUEST['telefone'];
+    echo "|E-mail: " . $_REQUEST['mail'];
+    echo "|Mensagem: " . $_REQUEST['mensagem'] . "|";
+?>

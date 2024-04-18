c935e613735102f1d326e6de9eee32df726f0307
Iago Trentini Etur <iago@arealocal.com.br>
2024-04-18 19:06:08 -0300

    Update consumo

--- laravel/consumo/resources/views/index.blade.php
+++ laravel/consumo/resources/views/index.blade.php
@@ -11,6 +11,10 @@
             </select><br/>
             <label id="lprecolitro" for="precolitro">Preço por litro:</label>
             <input type="number" step="0.01" name="precolitro"><br/>
+            <label id="ldistancia" for="distancia">Distância em quilometros a ser percorrida:</label>
+            <input type="number" name="distancia"><br/>
+            <label id="lconsumocombustivel" for="consumocombustivel">Consumo de combustível do veículo (km/l)</label>
+            <input type="number" name="consumocombustivel"><br/>
         </form>
     </div>
 @endsection
